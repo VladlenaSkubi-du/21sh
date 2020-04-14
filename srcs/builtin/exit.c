47b36af13bb4d64becbbb07c73db7fec4c6bac25
kfalia-f <unstableperson@yandex.ru>
2020-04-14 14:01:05 +0300

    fixed exit sega

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -9,19 +9,24 @@ int				btin_exit(t_ltree *pos)
 
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	i = 0;
-	while (pos->ar_v[1][i])
+	if (pos->ar_c > 1)
 	{
-		if (!(pos->ar_v[1][i] >= '0' && pos->ar_v[1][i] <= '9'))
+		while (pos->ar_v[1][i])
 		{
-			str = ft_strjoin(pos->ar_v[0], ": ");
-			str = ft_strrejoin(str, pos->ar_v[1]);
-			error_handler(SYNTAX_ERROR | (ERR_NUMERIC << 9), str);
-			free(str);
-			break ;
+			if (!(pos->ar_v[1][i] >= '0' && pos->ar_v[1][i] <= '9'))
+			{
+				str = ft_strjoin(pos->ar_v[0], ": ");
+				str = ft_strrejoin(str, pos->ar_v[1]);
+				error_handler(SYNTAX_ERROR | (ERR_NUMERIC << 9), str);
+				free(str);
+				break ;
+			}
+			i++;
 		}
-		i++;
+		status = ft_atoi(pos->ar_v[1]);
 	}
-	status = ft_atoi(pos->ar_v[1]);
+	else
+		status = 0;
 	ft_one_ltree_clear(pos); //почистить все листы
 	fill_hist_in_file();
 	clean_everything();
